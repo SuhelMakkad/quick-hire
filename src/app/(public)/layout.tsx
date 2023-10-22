9ff015187cd31cd87efb96291a1790ced12fb6cf
unknown <makadsuhel11@gmail.com>
2023-10-22 12:47:34 +0530

    seprated public and admin layout

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/nav-bar";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             enableSystem
             disableTransitionOnChange
           >
-            <Navbar />
             {children}
           </ThemeProvider>
         </QueryProvider>
