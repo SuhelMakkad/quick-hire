6567fb219ab687bf4f1db445fba66263408100fc
unknown <makadsuhel11@gmail.com>
2023-10-23 21:07:59 +0530

    minor ui updates for application page

--- src/app/(admin)/admin/application/[id]/page.tsx
+++ src/app/(admin)/admin/application/[id]/page.tsx
@@ -7,6 +7,7 @@ import { buttonVariants } from "@/components/ui/button";
 import { getJobDetailsRoute, getResumeServerPath } from "@/utils/routes";
 import getApplicationDetails from "@/lib/get-application-details";
 import getApplicationIds from "@/lib/get-application-ids";
+import { getJobDetails } from "@/lib/get-Job-details";
 
 export type ApplicationPageProps = {
   params: {
@@ -32,28 +33,54 @@ const ApplicationPage = async ({ params }: ApplicationPageProps) => {
     notFound();
   }
 
-  return (
-    <main className="container my-4">
-      <h1 className="font-medium md:text-2xl text-lg capitalize">
-        {application.firstName} {application.lastName}
-      </h1>
+  const job = await getJobDetails(application.jobId);
 
-      <span className="text-xs md:text-sm text-muted-foreground">
-        {new Date(application.timestamp).toLocaleString("en-US")}
-      </span>
+  return (
+    <main className="container my-8 max-w-lg mx-auto">
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="font-medium md:text-2xl text-lg capitalize">
+            {application.firstName} {application.lastName}
+          </h1>
+
+          <span className="text-xs md:text-sm text-muted-foreground">
+            {new Date(application.timestamp).toLocaleString("en-US")}
+          </span>
+        </div>
+
+        <Link
+          target="_blank"
+          href={getResumeServerPath(application.id)}
+          className={buttonVariants({
+            size: "sm",
+            className: "flex items-center gap-2",
+          })}
+          download
+        >
+          <Download className="w-4" />
+          Resume
+        </Link>
+      </div>
 
       <section className="flex flex-col mt-8 space-y-4">
         <h2 className="font-medium text-xs md:text-sm">Basic Details</h2>
 
         <div className="flex flex-col gap-2">
-          <Link
-            target="_blank"
-            href={getJobDetailsRoute(application.jobId)}
-            className="hover:underline text-sm md:text-base flex items-center gap-2"
-          >
-            <Send className="w-4" />
-            Applied for {application.jobId}
-          </Link>
+          {job ? (
+            <Link
+              target="_blank"
+              href={getJobDetailsRoute(application.jobId)}
+              className="hover:underline text-sm md:text-base flex items-center gap-2"
+            >
+              <Send className="w-4" />
+              Applied for {job.title}
+            </Link>
+          ) : (
+            <span className="text-sm md:text-base flex items-center gap-2">
+              <Send className="w-4" />
+              Job Not found!
+            </span>
+          )}
 
           <Link
             target="_blank"
@@ -94,21 +121,6 @@ const ApplicationPage = async ({ params }: ApplicationPageProps) => {
               {application.github}
             </Link>
           )}
-        </div>
-
-        <div>
-          <Link
-            target="_blank"
-            href={getResumeServerPath(application.id)}
-            className={buttonVariants({
-              size: "sm",
-              className: "flex items-center gap-2",
-            })}
-            download
-          >
-            <Download className="w-4" />
-            Resume
-          </Link>
         </div>
       </section>
     </main>
