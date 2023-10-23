d6ccef52d283f2a9bbb02950f2ceebb6d8ad8fb6
unknown <makadsuhel11@gmail.com>
2023-10-23 19:09:56 +0530

    removed status cards for application admin page

--- src/app/(admin)/admin/components/overview-cards.tsx
+++ src/app/(admin)/admin/components/overview-cards.tsx
@@ -15,8 +15,6 @@ const OverviewSection = ({}: OverviewSectionProps) => {
     return (
       <div className="flex lg:grid gap-4 grid-cols-4 overflow-auto hide-scroll">
         <LoadingCard />
-        <LoadingCard />
-        <LoadingCard />
       </div>
     );
   }
@@ -36,26 +34,6 @@ const OverviewSection = ({}: OverviewSectionProps) => {
         </CardHeader>
         <CardContent>
           <div className="text-xl md:text-2xl font-medium">{applications.total}</div>
-        </CardContent>
-      </Card>
-
-      <Card className="min-w-[16rem]">
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">New</CardTitle>
-          <BarChart2 className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <div className="text-xl md:text-2xl font-medium">{applications.new}</div>
-        </CardContent>
-      </Card>
-
-      <Card className="min-w-[16rem]">
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Shortlisted</CardTitle>
-          <CheckCircle2 className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <div className="text-xl md:text-2xl font-medium">{applications.shortlisted}</div>
         </CardContent>
       </Card>
     </div>
