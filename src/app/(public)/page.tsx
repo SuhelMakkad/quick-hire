a616f09cf5b482fafbbb063ef8cf9d0c63fdc8f8
unknown <makadsuhel11@gmail.com>
2023-10-24 00:18:05 +0530

    removed job count

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
     <main className="container">
       <header className="mb-4 md:mb-5">
         <h1 className="font-medium text-lg md:text-xl">Best jobs around the world</h1>
-        <span className="text-xs md:text-sm">365 Open roles</span>
       </header>
 
       <JobList />
