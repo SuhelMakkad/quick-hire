75ba80995423024788ffaa6264601a7a31f59429
unknown <makadsuhel11@gmail.com>
2023-10-24 00:22:22 +0530

    added page loading bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
 
 import { ThemeProvider } from "./components/theme-provider";
 import { QueryProvider } from "./components/query-client";
@@ -26,6 +27,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             enableSystem
             disableTransitionOnChange
           >
+            <NextTopLoader showSpinner={false} />
+
             {children}
 
             <Toaster />
